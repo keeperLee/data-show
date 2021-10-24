58c12154eeb2b415dd76c31d8a357870642ff4da
lijian <ljhhh@qq.com>
2021-10-25 01:29:08 +0800

    添加测试路由

---
+++ src/views/Test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="test">Test-{{message}}</div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data () {
+    return {
+        message: 'hello world'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .test{
+    color: red
+  }
+</style>
