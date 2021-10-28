bc9d182f704e84c7b61af36098a9278963417986
lijian <ljhhh@qq.com>
2021-10-28 23:44:08 +0800

    使用vuex的用法和vue2差不多

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,41 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    number: 100
   },
   mutations: {
+    SET_NUMBER (state, value) {
+      state.number = value
+    }
   },
   actions: {
+    setNumber ({ commit }, value) {
+      commit('SET_NUMBER', value)
+    }
   },
   modules: {
+    a: {
+      state: {
+        aa: 11
+      },
+      mutations: {
+        SET_AA (state, value) {
+          state.aa = value
+        }
+      }
+    },
+    b: {
+      state: {
+        bb: 22
+      },
+      mutations: {
+        SET_BB (state, value) {
+          state.bb = value
+        }
+      }
+    }
+  },
+  getters: {
+    aaValue: (state) => state.a.aa
   }
 })
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -5,6 +5,15 @@
     <div>doubleCount:{{ doubleCount }}</div>
     <button @click="increment">增加</button>
     <button @click="goToAbout">跳转到about</button>
+    <div>number:{{ number }}</div>
+    <button @click="updateNumber">update number</button>
+    <div>aa:{{ aa }}</div>
+    <div>bb:{{ bb }}</div>
+    <div @click="updateAA">updateAA</div>
+    <div>getters aa:{{
+        gettersAA
+      }}
+    </div>
   </div>
 </template>
 
@@ -29,6 +38,19 @@ export default {
     })
 
     const doubleCount = computed(() => count.value * 2)
+    const number = computed(() => proxy.$store.state.number)
+    const aa = computed(() => proxy.$store.state.a.aa)
+    const bb = computed(() => proxy.$store.state.b.bb)
+    const gettersAA = computed(() => proxy.$store.getters.aaValue)
+
+    const updateNumber = () => {
+      // proxy.$store.commit('SET_NUMBER', count.value * 50)
+      proxy.$store.dispatch('setNumber', count.value * 50)
+    }
+    const updateAA = () => {
+      // proxy.$store.commit('SET_NUMBER', count.value * 50)
+      proxy.$store.commit('SET_AA', proxy.$store.state.a.aa * 50)
+    }
 
     const goToAbout = () => {
       proxy.$router.push('/about')
@@ -41,7 +63,13 @@ export default {
       count,
       increment,
       doubleCount,
-      goToAbout
+      goToAbout,
+      number,
+      updateNumber,
+      aa,
+      bb,
+      updateAA,
+      gettersAA
     }
   }
 }
