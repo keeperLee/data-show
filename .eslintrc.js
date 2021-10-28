520803defcb629b2dcb7897ffe184d70a0128cae
lijian <1376623234@qq.com>
2021-10-28 10:31:37 +0800

    vue3引入echarts5

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,11 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'indent': 'off'
+    'no-console':  'off',
+    'no-debugger':  'off',
+    'indent': 'off',
+    'no-irregular-whitespace' : 'off',
+    'space-before-function-paren': 'off',
+    'semi': 'off'
   }
 }
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,20 +1,43 @@
 <template>
-  <div class="test">Test-{{message}}</div>
+  <div class="test">Test-{{ message }}</div>
+  <div id="container" style="width:400px;height: 400px"></div>
 </template>
 
 <script>
 export default {
   name: 'Test',
-  data () {
+  data() {
     return {
-        message: 'hello world'
+      message: 'hello world'
+    }
+  },
+  mounted() {
+    this.initChart();
+  },
+  methods: {
+    initChart() {
+      const dom = document.getElementById('container');
+      const chart = this.$echarts.init(dom);
+      chart.setOption({
+        title: {
+          text: 'Echarts 入门案例'
+        },
+        xAxis: {
+          data: ['食品', '数码', '服饰', '箱包']
+        },
+        yAxis: {},
+        series: {
+          type: 'bar',
+          data: [100, 200, 90, 150]
+        }
+      });
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  .test{
-    color: red
-  }
+.test {
+  color: red
+}
 </style>
