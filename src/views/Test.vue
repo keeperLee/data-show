df112d91343753f4eb13ec79326682e28644377c
lijian <ljhhh@qq.com>
2021-10-28 23:10:57 +0800

    使用ref,watch,computed,getCurrentInstance()

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,27 +1,47 @@
 <template>
-  <div class="test">Test-{{ message }}</div>
+  <!--  <div class="test">Test-{{ message }}{{ state.count }}-->
+  <div class="test">
+    <div>count:{{ count }}</div>
+    <div>doubleCount:{{ doubleCount }}</div>
+    <button @click="increment">增加</button>
+    <button @click="goToAbout">跳转到about</button>
+  </div>
 </template>
 
 <script>
-import { reactive, watchEffect } from 'vue'
-// 类似 react hooks 中的useState
-const state = reactive({
-  count: 1
-})
+import { ref, computed, watch, getCurrentInstance } from 'vue'
 
-const increment = () => {
-  state.count++
-}
-// 类似 react hoos中的 useEffect
-watchEffect(() => {
-  document.body.innerHTML = `count: ${state.count}`
-  document.body.addEventListener('click', increment)
-})
 export default {
   name: 'Test',
-  data () {
+  setup () {
+    // 类似 react hooks 中的useState
+    // const state = reactive({
+    //   count: 1
+    // })
+    // console.log(this); // 无法获取到this
+    const { proxy } = getCurrentInstance()
+    console.log(proxy.$root.$router)
+
+    const count = ref(0)
+
+    watch(count, val => {
+      console.log(`count is ${val}`)
+    })
+
+    const doubleCount = computed(() => count.value * 2)
+
+    const goToAbout = () => {
+      proxy.$router.push('/about')
+    }
+
+    const increment = () => {
+      count.value++
+    }
     return {
-      message: 'hello world'
+      count,
+      increment,
+      doubleCount,
+      goToAbout
     }
   }
 }
